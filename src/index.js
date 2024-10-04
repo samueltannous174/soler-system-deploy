ac81f90fcac307d64e60defc3278f8aa46ae0872
samueltannous <106975094+samueltannous174@users.noreply.github.com>
2024-10-04 23:07:25 +0300

    updates

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <BrowserRouter basename={"/soler-system-deplo"}>
+        <BrowserRouter basename={"/soler-system-deploy"}>
             <Routes>
                 <Route path="/" element={<App />} />
                 <Route path="/earth" element={<EarthPlanet />} />
